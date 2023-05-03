93673530a5d38132af7498035038a222d79b860c
BaSsel Ahmed <88636331+Bensa0@users.noreply.github.com>
2023-05-03 04:43:15 +0300

    sobel filtered

--- edge_detection.py
+++ edge_detection.py
@@ -19,7 +19,7 @@ def create_frames_from_video(video_Path):
         ret, frame = video.read()
 
         if ret:
-            # frame = cv2.resize(frame, (500, 500))
+            frame = cv2.resize(frame, (500, 500))
             frames_list.append(frame)
             binary_edge_map = create_sobel_filter_for_image(image=frame, x_kernel= 3, y_kernel=3, threshold_value=100)
             # laplacian = cv2.Laplacian(frame, cv2.CV_64F)
@@ -47,26 +47,24 @@ def show_video_from_frames(frames_list, filtered_frames_list,morph_frames_list):
     cv2.destroyAllWindows()
 
 
-def create_sobel_filter_for_image(image, x_kernel=3, y_kernel=3, threshold_value = 100):
-    # convert image to gray scale 
+def create_sobel_filter_for_image(image, x_kernel=3, y_kernel=3, threshold_value=100, sigma=1):
+    # convert image to gray scale
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    # gray = image
-    # As the Kernel size increases, more pixels are now a part of the convolution process.
-    # This signifies that the gradient map (edges) will tend to get blurry to a point the 
-    # output looks likes a plastic cover has been wrapped around the edges.
-    
-    # define kernel for filter
+
+    # Apply Gaussian smoothing to the image
+    gray = cv2.GaussianBlur(gray, (x_kernel, y_kernel), sigma)
+
     # Calculate gradient using Sobel kernels
     grad_x = cv2.Sobel(gray, cv2.CV_64F, 1, 0, ksize=x_kernel)
     grad_y = cv2.Sobel(gray, cv2.CV_64F, 0, 1, ksize=y_kernel)
 
     # Calculate gradient magnitude
     grad_mag = cv2.magnitude(grad_x, grad_y)
-     
-    # reducing the threshold increases the edges
+
     # Apply thresholding to the gradient magnitude image
     _, binary_edge_map = cv2.threshold(grad_mag, threshold_value, 255, cv2.THRESH_BINARY)
     return np.uint8(binary_edge_map)
+
 
 def create_Mathematical_Morphology_for_image(img):
     # Define a kernel for morphological operations
