be3f34b09a2b73b494fcf513fe4b0744c100b627
ZeyadMashhour <ZeyadMashhour84@gmail.com>
2023-05-03 06:06:26 +0300

    Summary

--- edge_detection.py
+++ edge_detection.py
@@ -9,7 +9,7 @@ train_path = r"BSDS500\data\images\train"
 validation_path = r"BSDS500\data\images\val"
 
 
-def create_frames_from_video(video_Path, sobel_threshold_value, morphology_theshold_value):
+def create_frames_from_video(video_Path, sobel_threshold_value = 100, ksize = 3):
     video = cv2.VideoCapture(video_Path)
     frames_list = []
     filtered_frames_list = []
@@ -21,60 +21,67 @@ def create_frames_from_video(video_Path, sobel_threshold_value, morphology_thesh
         if ret:
             frame = cv2.resize(frame, (500, 500))
             frames_list.append(frame)
-            binary_edge_map = create_sobel_filter_for_image(image=frame, x_kernel= 3, y_kernel=3, threshold_value=sobel_threshold_value)
-            # laplacian = cv2.Laplacian(frame, cv2.CV_64F)
-            # laplacian = np.uint8(laplacian)
-            #filtered_frame = cv2.resize(binary_edge_map, (960, 540))
-
-            morphImg = create_Mathematical_Morphology_for_image(binary_edge_map)
-            
-
+            binary_edge_map = perform_edge_detection(frame, ksize=ksize, threshold=sobel_threshold_value)
             filtered_frames_list.append(binary_edge_map)
-            morph_frames_list.append(morphImg)
         else:
             break
     video.release()
-    return frames_list, filtered_frames_list,morph_frames_list
+    return frames_list, filtered_frames_list#,morph_frames_list
 
 
-def show_video_from_frames(frames_list, filtered_frames_list,morph_frames_list):
+def show_video_from_frames(frames_list, filtered_frames_list):
     for i in range(len(filtered_frames_list)):
         cv2.imshow("frame", frames_list[i])
         cv2.imshow("filtered_frames", filtered_frames_list[i])
-        cv2.imshow("morhped_frames", morph_frames_list[i])
         if(cv2.waitKey(5) == ord('q')):
             break
     cv2.destroyAllWindows()
 
 
-def create_sobel_filter_for_image(image, x_kernel=3, y_kernel=3, threshold_value=100, sigma=1):
-    # convert image to gray scale
+def gray_scale(image):
+    # Convert the image to grayscale
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    return gray
 
-    # Apply Gaussian smoothing to the image
-    gray = cv2.GaussianBlur(gray, (x_kernel, y_kernel), sigma)
+def gaussian_blur(image):
+    # Apply a Gaussian blur to the image
+    blurred = cv2.GaussianBlur(image, (3, 3), 0)
+    return blurred
 
-    # Calculate gradient using Sobel kernels
-    grad_x = cv2.Sobel(gray, cv2.CV_64F, 1, 0, ksize=x_kernel)
-    grad_y = cv2.Sobel(gray, cv2.CV_64F, 0, 1, ksize=y_kernel)
+def sobel_edge_detection(image, ksize=3, threshold=50):
+    # Apply the Sobel edge detection algorithm
+    sobelx = cv2.Sobel(image, cv2.CV_64F, 1, 0, ksize=ksize)
+    sobely = cv2.Sobel(image, cv2.CV_64F, 0, 1, ksize=ksize)
+    magnitude = cv2.magnitude(sobelx, sobely)
 
-    # Calculate gradient magnitude
-    grad_mag = cv2.magnitude(grad_x, grad_y)
+    # Apply a threshold to the magnitude image
+    thresholded = np.zeros_like(magnitude)
+    thresholded[magnitude > threshold] = 255
 
-    # Apply thresholding to the gradient magnitude image
-    _, binary_edge_map = cv2.threshold(grad_mag, threshold_value, 255, cv2.THRESH_BINARY)
-    return np.uint8(binary_edge_map)
-
+    return thresholded
 
 
-def create_Mathematical_Morphology_for_image(img):
-    # Define a kernel for morphological operations
-    kernel = np.ones((5,5), np.uint8)
-    # Apply morphological closing to fill in small gaps in the edges
-    closing = cv2.morphologyEx(img, cv2.MORPH_CLOSE, kernel)
-    # Apply morphological opening to remove small objects and noise from the edges
-    opening = cv2.morphologyEx(closing, cv2.MORPH_OPEN, kernel)
-    return opening
+def morphological_operations(image):
+    # Apply mathematical morphology operations
+    kernel = np.ones((3, 3), np.uint8)
+    dilation = cv2.dilate(image, kernel, iterations=1)
+    erosion = cv2.erode(dilation, kernel, iterations=1)
+    return erosion
+
+def perform_edge_detection(image, ksize=3, threshold=50):
+    # Convert the image to grayscale
+    gray = gray_scale(image)
+
+    # Apply a Gaussian blur to the image
+    blurred = gaussian_blur(gray)
+
+    # Apply the Sobel edge detection algorithm
+    edges = sobel_edge_detection(blurred, ksize=ksize, threshold=threshold)
+
+    # Apply mathematical morphology operations
+    edges = morphological_operations(edges)
+    return edges
+
 
 def read_ground_truth_images(path, subscript=1):
     """
